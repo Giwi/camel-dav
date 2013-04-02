aaa49da3db89463fababfab26e1b3f9f21f63b5d
Giwi <giwi@free.fr>
2013-04-03 00:06:17 +0200

    code cleanup

--- src/test/java/org/giwi/camel/dav/test/DavThrowExceptionOnConnectionFailedTest.java
+++ src/test/java/org/giwi/camel/dav/test/DavThrowExceptionOnConnectionFailedTest.java
@@ -1,18 +1,17 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with this
- * work for additional information regarding copyright ownership. The ASF
- * licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Copyright 2013 Giwi Softwares (http://giwi.free.fr)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0 
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.giwi.camel.dav.test;
 
@@ -26,10 +25,16 @@ import org.junit.Test;
 
 /**
  * Unit test for login failure due bad password and no re connect attempts
- * allowed
+ * allowed.
  */
 public class DavThrowExceptionOnConnectionFailedTest extends AbstractDavTest {
 
+    /**
+     * Test bad login.
+     * 
+     * @throws Exception
+     *             the exception
+     */
     @Test
     public void testBadLogin() throws Exception {
 	try {
@@ -45,6 +50,16 @@ public class DavThrowExceptionOnConnectionFailedTest extends AbstractDavTest {
 	assertFalse("The file should NOT exists", file.exists());
     }
 
+    /**
+     * Upload file.
+     * 
+     * @param username
+     *            the username
+     * @param password
+     *            the password
+     * @throws Exception
+     *             the exception
+     */
     private void uploadFile(String username, String password) throws Exception {
 	Endpoint endpoint = context
 		.getEndpoint("dav://"
